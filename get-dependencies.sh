4390a6616dc0c3fef453b85127bb2128b22d498b
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2026-02-17 16:24:10 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -13,6 +13,8 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
+make-aur-package plutosvg
+make-aur-package plutovg
 make-aur-package pcsx2-git
 
 # If the application needs to be manually built that has to be done down here
