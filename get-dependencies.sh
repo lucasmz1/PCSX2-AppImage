d5def9d2a0e726d0cab911e392f7dc725869c5ab
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2026-02-17 11:44:19 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -13,7 +13,7 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package pcsx2
+make-aur-package pcsx2-git
 
 # If the application needs to be manually built that has to be done down here
 
