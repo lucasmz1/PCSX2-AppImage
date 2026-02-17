eedd9dcfdc44e5405b76dae62aa6f8858d552bd9
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2026-02-17 11:46:18 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,7 +8,7 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/pixmaps/pcsx2.png
+export ICON=/usr/share/icons/hicolor/512x512/apps/PCSX2.png
 export DESKTOP=/usr/share/applications/pcsx2.desktop
 
 # Deploy dependencies
