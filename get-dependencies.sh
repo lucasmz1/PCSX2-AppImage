1e879ff6dcf1365fb7cb700a881ec6b3fc52f887
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2026-02-18 11:15:46 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -13,6 +13,7 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
+make-aur-package shaderc-git
 make-aur-package --chaotic-aur pcsx2-git
 
 # If the application needs to be manually built that has to be done down here
