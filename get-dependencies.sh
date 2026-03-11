e4703f1860fdad5978c417b736e7a714f6ca13bd
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-03-11 18:53:18 -0400

    add several platformthemes (#3)

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm shaderc
+pacman -Syu --noconfirm shaderc qt6ct kvantum lxqt-qtplugin
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
